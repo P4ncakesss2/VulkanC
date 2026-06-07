4017ea7522448d04d98c3863ba28788480ba47d5
Wojciech Pawlowicz <wojciechpawlowicz992@gmail.com>
2026-06-07 23:18:18 +0200

    Fix .gitignore and untrack build folder

--- build/CMakeFiles/fc-tmp/cglm/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for cglm")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/pancake/CLionProjects/VulkanC/build/CMakeFiles/fc-tmp/cglm/cglm-gitclone.cmake")
-
-endblock()
--- build/CMakeFiles/fc-tmp/cglm/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for cglm")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/pancake/CLionProjects/VulkanC/build/CMakeFiles/fc-tmp/cglm/cglm-gitupdate.cmake")
-
-endblock()
